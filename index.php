0e57315251352dcaf7d7fcf4ee48631c2e714b80
Niclass <niclas.soder@gmail.com>
2017-04-06 14:09:12 +0200

    hej

--- index.php
+++ index.php
@@ -6,5 +6,18 @@ use Monolog\Handler\StreamHandler;
 
 $log = new Logger('Laboration 1');
 $log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
-$log->info("Hello $argv[1]! hejhejehj");
+
+$name = $_GET['name'];
+$log->info($name);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testing, testing</title>
+</head>
+<body>
+<?php
+echo "Hello, " . $name;
+?>
+</body>
+</html>
